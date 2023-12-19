96483247575a67a1e9edee66e614853046dd7ce2
jbarcossc <juancruzbarcos@hotmail.com>
2023-12-19 20:59:18 -0300

    Add icons and fix bugs

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import '../styles/Project.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
 class Project extends Component {
+    constructor(props) {
+        super(props);
+        library.add(fab);
+    };
     render() {
         const imageUrl = process.env.PUBLIC_URL + '/images/' + this.props.image;
-
         return (
             <a id={this.props.id} className="project-tile" href={this.props.href} target='_blank' rel="noreferrer">
-                <div className="project-image">
-                    <img src={imageUrl} alt={this.props.title + " project"} />
+                <div id="project-stack">
+                    {Object.values(this.props.stack).map(element => (
+                        <FontAwesomeIcon icon={['fab',element]} className="tech-icon" key={element}/>
+                    ))}
                 </div>
-                <div className="project-link">
-                    <span className="project-link-detail">&lt;</span>{this.props.title}<span className="project-link-detail">/&gt;</span>
+                <div className='project-info'>
+                    <div className="project-image">
+                        <img src={imageUrl} alt={this.props.title + " project"} />
+                    </div>
+                    <div className="project-link">
+                        <span className="project-link-detail">&lt;</span>{this.props.title}<span className="project-link-detail">/&gt;</span>
+                    </div>
                 </div>
             </a>
         );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,10 +4,10 @@ import Project from './Project';
 
 class Projects extends Component {
     projects_info = {
-        0: { id: "project-survey", image: "survey-form.png", href:"https://mattcoder3.github.io/survey-form/", title: "Survey Form"},
-        1: { id: "project-tribute", image: "tribute-page.png", href:"https://mattcoder3.github.io/tribute-page/", title: "Tribute Page"},
-        2: { id: "project-documentation", image: "documentation-page.png", href:"https://mattcoder3.github.io/technical-documentation-page/", title: "Documentation Page"},
-        3: { id: "project-landing", image: "landing-page.png", href:"https://mattcoder3.github.io/landing-page/", title: "Landing Page"}
+        0: { id: "project-survey", image: "survey-form.png", href:"https://mattcoder3.github.io/survey-form/", title: "Survey Form", stack: ["html5","css3-alt","git-alt"]},
+        1: { id: "project-game-of-life", image: "game-of-life.png", href:"https://jbarcossc.github.io/gameOfLife/", title: "Game of Life", stack: ["html5","css3-alt","js","git-alt"]},
+        2: { id: "project-documentation", image: "documentation-page.png", href:"https://mattcoder3.github.io/technical-documentation-page/", title: "Documentation Page", stack: ["html5","css3-alt","git-alt"]},
+        3: { id: "project-landing", image: "landing-page.png", href:"https://mattcoder3.github.io/landing-page/", title: "Landing Page", stack: ["html5","css3-alt","git-alt"]}
     };
 
     render(){
@@ -15,7 +15,7 @@ class Projects extends Component {
             <section id="projects">
                 <div id="projects-projects">
                     {Object.values(this.projects_info).map(element => (
-                            <Project key={element.id} id={element.id} image={element.image} href={element.href} title={element.title}/>
+                            <Project key={element.id} id={element.id} image={element.image} href={element.href} title={element.title} stack={element.stack}/>
                     ))}
                 </div>
                 <div id="profile-link-container">
