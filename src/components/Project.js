d57cd337bb366b3f77d44a703f0507d9f0457ac0
jbarcossc <juancruzbarcos@hotmail.com>
2024-04-12 20:05:57 -0300

    Update icon method

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,38 +1,46 @@
 import React, { Component } from 'react';
 import '../styles/Project.css';
-import { AiFillAccountBook } from "react-icons/ai";
-
-const DynamicIcon = ({ iconName }) => {
-  const Icon = React.lazy(() => import(`react-icons/${iconName}`));
-
-  return (
-    <React.Suspense fallback={<div>Loading...</div>}>
-      <Icon />
-    </React.Suspense>
-  );
-};
+import { ICONS } from '../icons';
 
 class Project extends Component {
-    render() {
-        const imageUrl = process.env.PUBLIC_URL + '/images/' + this.props.image;
-        return (
-            <a id={this.props.id} className="project-tile" href={this.props.href} target='_blank' rel="noreferrer">
-                <div id="project-stack">
-                    {Object.values(this.props.stack).map(element => (
-                        <DynamicIcon iconName={element} />
-                    ))}
-                </div>
-                <div className='project-info'>
-                    <div className="project-image">
-                        <img src={imageUrl} alt={this.props.title + " project"} />
-                    </div>
-                    <div className="project-link">
-                        <span className="project-link-detail">&lt;</span>{this.props.title}<span className="project-link-detail">/&gt;</span>
-                    </div>
-                </div>
-            </a>
-        );
-    }
+	render() {
+		const imageUrl =
+			process.env.PUBLIC_URL + '/images/' + this.props.image;
+		return (
+			<a
+				id={this.props.id}
+				className="project-tile"
+				href={this.props.href}
+				target="_blank"
+				rel="noreferrer">
+				<div id="project-stack">
+					{Object.values(this.props.stack).map((element, index) => (
+						<svg
+							key={`${element.iconName}-${element.id}-${index}`}
+							role="img"
+							viewBox="0 0 24 24"
+							xmlns="http://www.w3.org/2000/svg">
+							<title>{element.iconName}</title>
+							<path d={ICONS[element.iconName]} />
+						</svg>
+					))}
+				</div>
+				<div className="project-info">
+					<div className="project-image">
+						<img
+							src={imageUrl}
+							alt={this.props.title + ' project'}
+						/>
+					</div>
+					<div className="project-link">
+						<span className="project-link-detail">&lt;</span>
+						{this.props.title}
+						<span className="project-link-detail">/&gt;</span>
+					</div>
+				</div>
+			</a>
+		);
+	}
 }
 
 export default Project;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,27 +3,64 @@ import '../styles/Projects.css';
 import Project from './Project';
 
 class Projects extends Component {
-    projects_info = {
-        0: { id: "project-goles-de-river", image: "goles-de-river.png", href:"https://www.upwork.com/freelancers/~01f73c965c867f046d?p=1713013410095878144", title: "Goles de River", stack: ["js","git-alt"]},
-        1: { id: "project-game-of-life", image: "game-of-life.png", href:"https://jbarcossc.github.io/gameOfLife/", title: "Game of Life", stack: ["html5","css3-alt","js","git-alt"]},
-        2: { id: "project-rappi-item-finder", image: "rappi-item-finder.png", href:"https://www.youtube.com/watch?v=vsTe8nuCoIY&ab_channel=MattCoder", title: "Rappi Item Finder", stack: ["python","git-alt"]},
-        3: { id: "project-flappy-racoon", image: "flappy-racoon.png", href:"https://www.youtube.com/watch?v=E-_UWV7NUFw&t=1s&ab_channel=MattCoder", title: "Flappy Racoon", stack: ["html5","css3-alt","git-alt"]}
-    };
+	projects_info = {
+		0: {
+			id: 'project-goles-de-river',
+			image: 'goles-de-river.png',
+			href: 'https://www.upwork.com/freelancers/~01f73c965c867f046d?p=1713013410095878144',
+			title: 'Goles de River',
+			stack: ['javascript', 'git'],
+		},
+		1: {
+			id: 'project-game-of-life',
+			image: 'game-of-life.png',
+			href: 'https://jbarcossc.github.io/gameOfLife/',
+			title: 'Game of Life',
+			stack: ['html', 'css', 'javascript', 'git'],
+		},
+		2: {
+			id: 'project-rappi-item-finder',
+			image: 'rappi-item-finder.png',
+			href: 'https://www.youtube.com/watch?v=vsTe8nuCoIY&ab_channel=MattCoder',
+			title: 'Rappi Item Finder',
+			stack: ['python', 'git'],
+		},
+		3: {
+			id: 'project-flappy-racoon',
+			image: 'flappy-racoon.png',
+			href: 'https://www.youtube.com/watch?v=E-_UWV7NUFw&t=1s&ab_channel=MattCoder',
+			title: 'Flappy Racoon',
+			stack: ['html', 'css', 'git'],
+		},
+	};
 
-    render(){
-        return (
-            <section id="projects">
-                <div id="projects-projects">
-                    {Object.values(this.projects_info).map(element => (
-                            <Project key={element.id} id={element.id} image={element.image} href={element.href} title={element.title} stack={element.stack}/>
-                    ))}
-                </div>
-                <div id="profile-link-container">
-                    <a id="profile-link" target="_blank" rel="noreferrer" href="https://github.com/jbarcossc">Show all</a>
-                </div>
-            </section>
-        )
-    }
+	render() {
+		return (
+			<section id="projects">
+				<div id="projects-projects">
+					{Object.values(this.projects_info).map((element) => (
+						<Project
+							key={element.id}
+							id={element.id}
+							image={element.image}
+							href={element.href}
+							title={element.title}
+							stack={element.stack}
+						/>
+					))}
+				</div>
+				<div id="profile-link-container">
+					<a
+						id="profile-link"
+						target="_blank"
+						rel="noreferrer"
+						href="https://github.com/jbarcossc">
+						Show all
+					</a>
+				</div>
+			</section>
+		);
+	}
 }
 
 export default Projects;
